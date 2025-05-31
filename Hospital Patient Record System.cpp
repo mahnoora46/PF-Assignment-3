441e02a226621fbc2953779e108254f124fc7c1a
Mahnoor <mahnoor.am46@gmail.com>
2025-05-31 11:20:31 +0500

    Add files via upload

---
+++ Hospital Patient Record System.cpp
@@ -0,0 +1,6 @@
+﻿Build started at 02:29...
+1>------ Build started: Project: Hospital Patient Record System, Configuration: Debug x64 ------
+1>Hospital Patient Record System.cpp
+1>Hospital Patient Record System.vcxproj -> C:\Users\mahno\Desktop\Hospital Patient Record System\Hospital Patient Record System\x64\Debug\Hospital Patient Record System.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
+========== Build completed at 02:29 and took 03.697 seconds ==========
